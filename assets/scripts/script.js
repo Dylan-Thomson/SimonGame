304da585fd61bc6393ce1b5f7b3f98470b5685db
Dylan Thomson <djthomson88@gmail.com>
2017-09-18 15:18:09 -0400

    Working on fixing issue where game-controls blocks clickable area of game-btns

---
+++ assets/scripts/script.js
@@ -0,0 +1,18 @@
+// Document Ready
+$(function() {
+	$(".btn-upper-left").on("click", function() {
+		console.log(".btn-upper-left clicked");
+	});
+
+	$(".btn-lower-left").on("click", function() {
+		console.log(".btn-lower-left clicked");
+	});
+
+
+	$(".start-btn").on("click", function() {
+		console.log(".start-btn clicked");
+	});
+	$(".strict-btn").on("click", function() {
+		console.log(".strict-btn clicked");
+	});
+});
