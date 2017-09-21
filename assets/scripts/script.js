abd57fae4d242d2a9d762f7087d0615e9c285758
Dylan Thomson <djthomson88@gmail.com>
2017-09-21 08:56:16 -0400

    Disable board when not playable, addMoveToSequence when player has inputted current correct sequence, hangle winning the game

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,8 +7,10 @@ $(function() {
 var isOn = $("input:checkbox").is(":checked");
 var isStrict = false;
 var isStarted = false;
+var isBoardDisabled = false;
 var sequence = [];
 var placeInSequence = 0;
+var movesToWin = 5;
 var count = 0;
 var sounds = [
     new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
@@ -19,44 +21,38 @@ var sounds = [
 
 function initGameButtonListeners() {
     $(".btn-upper-left").on("click", function() {
-        if(isOn) {
+        if(isPlayable()) {
             console.log(".btn-upper-left clicked");
             playMove(0);
             compareMove(0);
-            placeInSequence++;
-            //compare with current sequence
         }
     });
     $(".btn-upper-right").on("click", function() {
-        if(isOn) {
+        if(isPlayable()) {
             console.log(".btn-upper-right clicked");
             playMove(1);
             compareMove(1);
-            placeInSequence++;
         }
     });
     $(".btn-lower-left").on("click", function() {
-        if(isOn) {
+        if(isPlayable()) {
             console.log(".btn-lower-left clicked");
             playMove(2);
             compareMove(2);
-            placeInSequence++;
         }
     });    
     $(".btn-lower-right").on("click", function() {
-        if(isOn) {
+        if(isPlayable()) {
             console.log(".btn-lower-right clicked");
             playMove(3);
             compareMove(3);
-            placeInSequence++;
-
         }
     });    
 }
 
 function initControlButtonListeners() {
     $(".start-btn").on("click", function() {
-        if(isOn) {
+        if(isPlayable()) {
             console.log(".start-btn clicked");
             isStarted = !isStarted;
             console.log("isStarted = " + isStarted);
@@ -67,7 +63,7 @@ function initControlButtonListeners() {
         }
     });
     $(".strict-btn").on("click", function() {
-        if(isOn) {
+        if(isPlayable()) {
             console.log(".strict-btn clicked");
             isStrict = !isStrict;
             console.log("isStrict = " + isStrict);
@@ -91,6 +87,7 @@ function initControlButtonListeners() {
 }
 
 function addMoveToSequence() {
+    console.log("addMoveToSequence()");
     sequence.push(Math.floor(Math.random() * 4));
     count++;
     placeInSequence = 0;
@@ -98,23 +95,53 @@ function addMoveToSequence() {
 }
 
 function playSequence() {
+    console.log("playSequence()", sequence);
+    isBoardDisabled = true;
     var counter = 0;
     var i = setInterval(function() {
         playMove(sequence[counter]);
+        // console.log(isPlayable());
         counter++;
         if(counter >= count) {
             clearInterval(i);
+            isBoardDisabled = false;
         }
     }, 1000);
 }
 
 function playMove(move) {
-    console.log("." + move);
+    console.log("playMove() ." + move);
     sounds[move].play();
+
 }
 
 function compareMove(move) {
+    console.log("compareMove()", move, sequence[placeInSequence]);
     if(move !== sequence[placeInSequence]) {
         console.log("YOU MESSED UP", move, sequence[placeInSequence]);
+        if(isStrict) {
+            //reset count to zero, clear sequence, etc
+        }
+        else {
+            placeInSequence = 0;
+            playSequence();            
+        }
     }
+    else {
+        placeInSequence++;
+        if(sequence.length == movesToWin && placeInSequence > (sequence.length - 1)) {
+            console.log("Winner!");
+            sequence = [];
+            count = 0;
+            $(".start-btn").trigger("click");
+        }
+        else if(placeInSequence > (count - 1)) {
+            console.log("placeInSequence = " + placeInSequence + " count-1 = " + (count - 1));
+            addMoveToSequence();
+        }       
+    }
+}
+
+function isPlayable() {
+    return isOn && !isBoardDisabled;
 }
