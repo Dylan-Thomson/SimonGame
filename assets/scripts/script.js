855a18c0d53db1d0c220289f203afe7ef8a6592a
Dylan Thomson <djthomson88@gmail.com>
2017-09-27 13:03:35 -0400

    Disable mute for Howler

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -98,15 +98,15 @@ function initControlButtonListeners() {
             console.log("on", isOn);
 
             isOn = false;
-            Howler.mute(true);
-            sounds.forEach(function(sound) {
+            // Howler.mute(true);
+            // sounds.forEach(function(sound) {
                 // sound.play();
-            });
+            // });
 
             var counter = 0;
             var startInterval = setInterval(function() {
                 // sounds[counter].mute(false);
-                Howler.mute(false);
+                // Howler.mute(false);
                 sounds[counter].play();
                 flashLight(counter);
                 counter++;
